893bf6529ec2404e6f6df05f41b95bf035a71121
Tamires Musafir <tamiresbarbosarj@gmail.com>
2026-02-20 02:11:34 -0300

    [3 WIP]postgres added to project.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2026_02_08_234805) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -63,7 +66,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_08_234805) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
