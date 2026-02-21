add6d4c09233c7cf74580fc62d013099f57b9d9d
Tamires Musafir <tamiresbarbosarj@gmail.com>
2026-02-21 19:38:18 -0300

    routes.rb updated

--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,9 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  match "*path",     to: "errors#not_found", via: :all
+  match "*path",
+      to: "errors#not_found",
+      via: :all,
+      constraints: ->(req) { !req.path.start_with?("/rails") }
   
 end
