c9a21666548a59f4908001606b6c6210c3865007
Tamires Musafir <tamiresbarbosarj@gmail.com>
2026-02-21 19:34:37 -0300

    routes.rb updated

--- config/routes.rb
+++ config/routes.rb
@@ -23,8 +23,6 @@ Rails.application.routes.draw do
   get    "login",    to: "sessions#new"
   post   "login",    to: "sessions#create"
   delete "logout",   to: "sessions#destroy"
-
-  match "*path",     to: "errors#not_found", via: :all
   
   # get "blog",             to: "posts#index" 
   # get "members",          to: "members#index"
@@ -43,4 +41,7 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
+  match "*path",     to: "errors#not_found", via: :all
+  
 end
